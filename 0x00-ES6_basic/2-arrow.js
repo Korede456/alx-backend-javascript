47e463a1a6995a13ee75365d80a191f9f9d47566
Korede Odeyemi <koredethompson456@gmail.com>
2024-09-30 11:32:48 -0700

    es6

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighorhoodsList() {
   this.sanFranciscoNeighorhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = () => {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
