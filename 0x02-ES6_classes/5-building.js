e795a133ca2184e486bc25568a79fd986c4bcfe4
korede456 <koredethompson456@gmail.com>
2024-10-13 10:50:26 +0100

    Javascript classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -5,13 +5,13 @@ export default class Building {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
           'Class extending Building must override evacuationWarningMessage',
-	);
+        );
       }
     }
   }
 
   get sqft() {
-  return this._sqft:
+    return this._sqft;
   }
 
   set sqft(value) {
